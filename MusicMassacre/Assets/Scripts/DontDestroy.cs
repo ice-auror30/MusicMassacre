42459af9602b3fbd80899564388c64e950d499e9
Shikhar Vats <svat@iastate.edu>
2015-10-15 19:04:08 -0500

    added demo1 files

---
+++ MusicMassacre/Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DontDestroy : MonoBehaviour {
+
+	void Awake () {
+		DontDestroyOnLoad (gameObject);
+	}
+}
---
+++ MusicMassacre/Assets/Scripts/GoToLevel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToLevel : MonoBehaviour {
+
+	public GameObject loadingImage;
+
+	public void LoadLevelOnClick(int level) {
+		if (loadingImage)
+			loadingImage.SetActive (true);
+
+		Application.LoadLevel (level);
+	}
+}
