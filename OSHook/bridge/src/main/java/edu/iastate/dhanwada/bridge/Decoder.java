553b95b62959751ecf8440271e340070c6ad7652
Shikhar Vats <svat@iastate.edu>
2015-11-08 18:47:02 -0600

    added the interface for wave decoder

--- OSHook/bridge/src/main/java/edu/iastate/dhanwada/bridge/Decoder.java
+++ OSHook/bridge/src/main/java/edu/iastate/dhanwada/bridge/Decoder.java
@@ -1,7 +1,14 @@
 package edu.iastate.dhanwada.bridge;
 
 /**
+ * interface for decoders to decode the input samples and transfer them to the fft for analysis
  * Created by IceAuror on 11/8/15.
  */
-public class Decoder {
+public interface Decoder {
+    /**
+     * reads the sample's length and returns the number of samples read.
+     * @param samples
+     * @return
+     */
+    public int readSamples(float []samples);
 }
