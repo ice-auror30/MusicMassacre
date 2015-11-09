24f30b6745e58b251b542d9aa34665f05d135c81
Shikhar Vats <svat@iastate.edu>
2015-11-08 19:14:47 -0600

    created the class to convert the data stream to float data

--- OSHook/bridge/src/main/java/edu/iastate/dhanwada/bridge/WaveDecoder.java
+++ OSHook/bridge/src/main/java/edu/iastate/dhanwada/bridge/WaveDecoder.java
@@ -1,7 +1,14 @@
 package edu.iastate.dhanwada.bridge;
 
-/**
+/** read in the audio strea and convert it to a signed 32-bit float.
  * Created by IceAuror on 11/8/15.
  */
-public class WaveDecoder {
+public class WaveDecoder implements Decoder{
+
+    private final float MAX_VALUE = 1.0f /Short.MAX_VALUE;
+    private final EndianDataInputStream in;
+    @Override
+    public int readSamples(float[] samples) {
+        return 0;
+    }
 }
